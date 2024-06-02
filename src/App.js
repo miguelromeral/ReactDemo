0d89867d3e5afb47de495a6dcfdbdb9be9966dfe
Miguel Romeral <miguel.romeral@gmail.com>
2024-06-02 10:48:18 +0200

    Creadas Variables de Entorno

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  console.log("API Key: "+process.env.REACT_APP_ENVIRONMENT);
+
   return (
     <div className="App">
       <header className="App-header">
