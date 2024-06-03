8ce44bf6f25496afb70df0b72ae31862aedefe30
Miguel Romeral <miguel.romeral@gmail.com>
2024-06-03 16:48:26 +0200

    Añadido un poco de Tailwind

--- src/components/screens/SearchMoviesScreen.jsx
+++ src/components/screens/SearchMoviesScreen.jsx
@@ -1,34 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback  } from 'react';
 import TmdbService from '../../services/TmdbService';
 import Movie from '../Movie'
 
 const SearchMoviesScreen = () => {
 
-  const initialQuery = 'Sonic';
+  const initialQuery = '';
 
   const [movies, setMovies] = useState([]);
   const [query, setQuery] = useState(initialQuery);
 
-  const search = async (event) => {
-    console.log(event.target.value);
+  const search = useCallback(async (event) => {
+    // console.log(event.target.value);
     setQuery(event.target.value);
 
-    setMovies(await TmdbService.searchMovies(query));
-  };
+    setMovies(query.length > 0 ?
+      await TmdbService.searchMovies(query) :
+      []);
+  }, [query]);
+
+  const listaMovies = movies.sort((a,b) => b.popularity - a.popularity).map((movie) => (
+    <Movie key={movie.id} movie={movie} />
+  ));
+
+  const notFound = <div>Nada por aquí 😓</div>
 
   return (
-    <>
-      <h1>Search Movies</h1>
+    <div class='grid gap-6 mb-6 gird-cols-1 mx-2'>
       <form>
-        <input type="text" name="movieQuery" value={query}
-          onChange={(event) => search(event)}/>
+        <div>
+            <label for="iMovieQuery" class="block mb-2 text-sm font-medium text-gray-900 ">Buscar Películas</label>
+            <input type="text" id="iMovieQuery" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Título de la Película" required
+            name="movieQuery" value={query}
+            onChange={(event) => search(event)} />
+        </div>
       </form>
       <div>
-        {movies.sort((a,b) => b.popularity - a.popularity).map((movie) => (
-          <Movie key={movie.id} movie={movie} />
-        ))}
+        {
+          movies.length > 0 ? listaMovies : notFound
+        }
       </div>
-    </>
+    </div>
   );
 };
 
