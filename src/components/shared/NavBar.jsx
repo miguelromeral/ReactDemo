4d1aae782aedf09d963a57a962e504add370fcca
Miguel Romeral <miguel.romeral@gmail.com>
2024-06-05 17:09:24 +0200

    Cambios en navbar

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -1,37 +1,59 @@
-import { Bars3Icon, HomeIcon, MagnifyingGlassIcon } from "@heroicons/react/16/solid";
+import {
+  Bars3Icon,
+  HomeIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/16/solid";
+import logo from "../../assets/images/logo512.png";
 
-function NavBar(){
-
+function NavBar() {
   return (
     <nav className="bg-blue-700 text-white flex justify-between">
-      <div>
-        <ul id="navBarMenu" className="hidden sm:flex flex-row flex-wrap transition ease-in-out">
+      <div className="flex flex-row justify-between w-full">
+        <ul>
           <li className="px-2 py-2 transition ease-in-out  hover:bg-blue-200 hover:text-blue-900">
             <a href="/" className="flex flex-row justify-center flex-nowrap">
-              <div className="h-auto flex flex-col justify-center pr-1">
-                <HomeIcon className="h-5" />
-              </div>
-              <span>Inicio</span>
-            </a>
-          </li>
-          <li className="px-2 py-2 transition ease-in-out  hover:bg-blue-200 hover:text-blue-900">
-            <a href="/search" className="flex flex-row justify-center flex-nowrap">
-              <div className="h-auto flex flex-col justify-center pr-1">
-                <MagnifyingGlassIcon className="h-5" />
-              </div>
-              <span>Buscar</span>
+              <img src={logo} className="h-6" />
+              <span className="px-2">React APP</span>
             </a>
           </li>
         </ul>
+        <div
+          id="navBarMenu"
+          className="hidden sm:flex"
+        >
+          <ul className="flex flex-col sm:flex-row">
+            <li className="px-2 py-2 transition ease-in-out  hover:bg-blue-200 hover:text-blue-900">
+              <a href="/" className="flex flex-row justify-center flex-nowrap">
+                <div className="h-auto flex flex-col justify-center pr-1">
+                  <HomeIcon className="h-5" />
+                </div>
+                <span>Inicio</span>
+              </a>
+            </li>
+            <li className="px-2 py-2 transition ease-in-out  hover:bg-blue-200 hover:text-blue-900">
+              <a
+                href="/search"
+                className="flex flex-row justify-center flex-nowrap"
+              >
+                <div className="h-auto flex flex-col justify-center pr-1">
+                  <MagnifyingGlassIcon className="h-5" />
+                </div>
+                <span>Buscar</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="px-2 py-2 block sm:hidden transition ease-in-out bg-blue-700 hover:bg-blue-200 text-white hover:text-blue-900">
-        <Bars3Icon 
-          className="h-6 cursor-pointer" 
-          onClick={() => { document.getElementById("navBarMenu").classList.toggle('hidden') }} />
+        <Bars3Icon
+          className="h-6 cursor-pointer"
+          onClick={() => {
+            document.getElementById("navBarMenu").classList.toggle("hidden");
+          }}
+        />
       </div>
     </nav>
-  )
+  );
 }
-
 
 export default NavBar;
